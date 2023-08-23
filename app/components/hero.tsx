88e0533f7b3bda56fdbcce427a654289ed7ca78c
ikhsanadrians <aikhsan032@gmail.com>
2023-08-23 18:10:47 +0700

    update hero text

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -10,7 +10,7 @@ export default function hero() {
         </Image>
         <div className="jumbotron-text flex flex-col gap-2 justify-center items-center h-full px-6">
             <h1 className="z-10 text-white md:text-5xl sm:text-4xl text-3xl lg:text-5xl font-[Mona] text-center">Welcome to Connexsoft</h1>
-            <p className="text-white md:w-1/2 w-full lg:w-1/2 text-center opacity-60">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem similique minus molestias perspiciatis vero obcaecati aliquam fuga unde. Corporis animi sequi reprehenderit possimus minima neque.</p>
+            <p className="text-white md:w-1/2 w-full lg:w-1/2 text-center opacity-60">Step into the world of innovation and exploration with our exceptional class at 10 Vocational High School Jakarta, specializing in ConnexSoft Software Engineering. Get ready to embark on a transformative journey that will shape your future in the tech world.</p>
         </div>
     </div>
   )
