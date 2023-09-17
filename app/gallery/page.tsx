38e2e4fe7f9ddf302d614fc4b3c9f6c750f82d42
ikhsanadrians <aikhsan032@gmail.com>
2023-09-18 04:45:52 +0700

    add gallery components

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,10 +1,117 @@
+"use client"
+
 import Navbar from "../components/navbar";
+import React, { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import { AiOutlineExpandAlt } from "react-icons/ai";
 
 const Gallery = () => {
-  return(
-     <>
-       <Navbar/>
-     </>
+  const [open, setOpen] = useState<boolean>(false);
+  const [image, setImage] = useState<string>("");
+  
+  type Gallery = {
+    imageUrl: string,
+    type: string,
+    title: string
+  }
+
+  const galleryTab : Gallery[] = [
+        {
+          imageUrl: "https://themewagon.github.io/snapshot/images/model-1.jpg",
+          type: "Nature",
+          title: "Beautiful Work",
+        },
+        {
+          imageUrl: "https://themewagon.github.io/snapshot/images/model-2.jpg",
+          type: "Nature",
+          title: "Beautiful Work",
+        },
+        {
+          imageUrl: "https://themewagon.github.io/snapshot/images/model-3.jpg",
+          type: "Nature",
+          title: "Beautiful Work",
+        },
+        {
+          imageUrl: "https://themewagon.github.io/snapshot/images/model-4.jpg",
+          type: "Nature",
+          title: "Beautiful Work",
+        },
+        {
+          imageUrl: "https://themewagon.github.io/snapshot/images/model-5.jpg",
+          type: "Nature",
+          title: "Beautiful Work",
+        },
+      ];
+
+      
+  const slides = galleryTab.map((item) => ({
+    src: item.imageUrl,
+    width: 3840,
+    height: 2560,
+    srcSet: [
+      { src: item.imageUrl, width: 320, height: 213 },
+      { src: item.imageUrl, width: 640, height: 426 },
+      { src: item.imageUrl, width: 1200, height: 800 },
+      { src: item.imageUrl, width: 2048, height: 1365 },
+      { src: item.imageUrl, width: 3840, height: 2560 },
+    ],
+  }));
+
+  console.log(image)
+
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto mt-6 lg:py-0 lg:px-0 py-4 px-8 pb-8 relative">
+        <h1 className="text-white font-[MonaReg] text-3xl lg:text-4xl">Photo Gallery</h1>
+        <p className="text-gray-400 w-full lg:w-1/2">Welcome to ConnexSoft's photo gallery, where we showcase a collection of captivating images. Explore our visual journey through these photos.</p>
+        <div className="w-full">
+        <div className=" ">
+          <div className="flex flex-col md:grid md:grid-cols-3 h-full gap-0 flex-wrap mx-2 md:mx-0">
+            {galleryTab.map((x, index) => {
+              return (
+                <div key={index} className="md:h-[50vw] h-screen relative">
+                  <div className="group h-full">
+                    <div
+                      className="bg-cover bg-center h-full w-full bg-no-repeat"
+                      style={{ backgroundImage: `url("${x.imageUrl}")` }}
+                    >
+                      <div className="text-3xl text-white absolute bottom-0 left-2 z-10">
+                        <div>{x.type}</div>
+                        <div>{x.title}</div>
+                      </div>
+                    </div>
+                    <div
+                      className="bg-black opacity-0 group-hover:opacity-75 absolute inset-0 flex items-center justify-center transition-all duration-300 ease-in-out"
+                      onClick={() => {
+                        setOpen(true);
+                        setImage(x.imageUrl); // Update the image state here
+                      }}
+                    >
+                      <p className="text-white">
+                        <AiOutlineExpandAlt className="text-5xl border w-16 h-16 bg-neutral-500 hover:bg-white hover:text-black p-3 cursor-pointer rounded-full" />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <Lightbox
+          index={galleryTab.findIndex((x) => x.imageUrl === image)}
+          open={open}
+          close={() => setOpen(false)}
+          plugins={[Zoom]}
+          slides={slides}
+
+        />
+      </div>
+      </div>
+
+    </>
   )
 }
 
