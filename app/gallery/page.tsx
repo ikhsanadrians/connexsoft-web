84f7db0a8025a699c0d381b0e7fd5aa2e678b2c1
ikhsanadrians <aikhsan032@gmail.com>
2023-09-23 19:49:12 +0700

    chore: fix padding-x in all pages

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -64,7 +64,7 @@ const Gallery = () => {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto mt-6 lg:py-0 lg:px-0 py-4 px-8 pb-8 relative">
+      <div className="container mx-auto mt-6 lg:py-0 lg:px-11 py-4 px-8 pb-8 relative">
         <h1 className="text-white font-[MonaReg] text-3xl lg:text-4xl">Photo Gallery</h1>
         <p className="text-gray-400 w-full lg:w-1/2">Welcome to ConnexSoft's photo gallery, where we showcase a collection of captivating images. Explore our visual journey through these photos.</p>
         <div className="w-full">
