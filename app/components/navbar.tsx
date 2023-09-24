90df42bd353d58fc5805aba8cf8f2ce5e774a0a7
ikhsanadrians <aikhsan032@gmail.com>
2023-09-24 23:01:01 +0700

    chore: add mobile nav menu

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,9 +6,15 @@ import LogoConnexsoft from "../../public/images/connexsoft.png";
 import { Roboto } from "next/font/google";
 import "../../public/css/app.css";
 import Link from "next/link";
+import { TiThMenu } from "react-icons/ti";
 
 function Navbar() {
   const [scrolled, setScrolled] = useState<boolean>(false);
+  const [navMobileBtn,setMobileNavBtn] = useState<boolean>(false);
+ 
+  const navMobileHandler = () => {
+    setMobileNavBtn(!navMobileBtn);
+  }
 
   useEffect(() => {
     const handleScroll = () => { 
@@ -23,6 +29,7 @@ function Navbar() {
   }, []);
 
   return (
+    <>
     <header className={`sticky top-0 z-99 ${scrolled && 'header drop-shadow-lg'}`}>
       <div className="header-wrapper container lg:px-0 md:px-0 px-6 mx-auto  w-full h-12 py-10 flex items-center justify-between">
         <div className="title flex items-center z-30">
@@ -40,8 +47,19 @@ function Navbar() {
             <li className="duration-200 hover:-translate-y-1"><Link href={"/projects"}>Project</Link></li>
           </ul>
         </div>
+        <TiThMenu onClick={navMobileHandler} color="white" size={25} className="block lg:hidden md:hidden"/>
       </div>
     </header>
+    <div className={`nav-btm ${navMobileBtn ? 'block' : 'hidden'} duration-100 bg-black w-full h-[20rem] sticky top-20 z-40 flex items-center`}>
+    <ul className="nav-links flex flex-col gap-6 font-medium text-gray-200 p-8">
+            <li className="duration-200 hover:-translate-y-1"><Link href={"/"}>Home</Link></li>
+            <li className="duration-200 hover:-translate-y-1"><Link href={"/members"}>Daftar Member</Link></li>
+            <li className="duration-200 hover:-translate-y-1"><Link href={"/gallery"}>Galleri</Link></li>
+            <li className="duration-200 hover:-translate-y-1"><Link href={"/articles"}>Artikel</Link></li>
+            <li className="duration-200 hover:-translate-y-1"><Link href={"/projects"}>Project</Link></li>
+          </ul>
+    </div>
+    </>
   );
 }
 
