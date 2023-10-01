9a2ea55a9f592df5477c38db515c80906ca70e35
ikhsanadrians <aikhsan032@gmail.com>
2023-10-01 09:34:11 +0700

    chore: add giscus & fix detail articles page

--- app/articles/[...articleSlug]/page.tsx
+++ app/articles/[...articleSlug]/page.tsx
@@ -5,32 +5,71 @@ import { useState, useEffect } from "react"
 import Article from "@/app/utils/types/articles"
 import axios from 'axios';
 import Image from "next/image";
+import Giscus from "@giscus/react";
+import Footer from "@/app/components/footer"
 
 
-const ArticleDetail = ({params} : {params: {articleSlug:string}}) => {
-   const [allArticle,setAllArticle] = useState<Article[]>([]);
-   
-   useEffect(()=>{
+
+const ArticleDetail = ({ params }: { params: { articleSlug: string } }) => {
+   const [allArticle, setAllArticle] = useState<Article[]>([]);
+
+   useEffect(() => {
       axios.get("https://connexsoft-team.github.io/api/v1/articles.json").
-      then((response)=>setAllArticle(response.data)).
-      catch((err)=>console.error(err))
-   },[])
+         then((response) => setAllArticle(response.data)).
+         catch((err) => console.error(err))
+   }, [])
 
 
 
    return (
       <>
-      <Navbar />
-      <div className="container mx-auto mt-6 lg:py-0 lg:px-11 py-4 px-8 pb-8 relative">
-        {
-            allArticle.filter((item)=>item.slug == params.articleSlug).map((item:Article,index:number)=>(
-               <>
-               <Image src={item.thumbnail} alt="thumbnail" width={100} height={100} />
-               <h1 className="text-white">{ item.title }</h1>
-               </>
-            ))
-        }
-      </div>
+         <Navbar />
+         <div className="container mx-auto mt-6 lg:py-0 lg:px-11 py-4 px-8 pb-8 relative">
+            {
+               allArticle.filter((item) => item.slug == params.articleSlug).map((item: Article, index: number) => (
+                  <div>
+                     <div className="image-content w-full h-80 overflow-hidden object-cover rounded-md">
+                        <img src={item.thumbnail} className="w-full h-full object-cover" />
+                     </div>
+                     <div className="text-content mt-4">
+                        <div className="content-title">
+                           <h1 className="text-white font-[MonaReg] text-3xl lg:text-4xl">{item.title}</h1>
+                           <span className="flex items-center gap-2 text-slate-400">Write By : <span className="flex gap-2 items-center"><Image src={item.author_img} height={16} width={16} alt="author-picture"/> {item.author}</span> / 3 min read / Posted In : {item.post_date}</span>
+                        </div>
+                        <div className="content-description">
+                           {
+                              item.description.map((desc: any, index: number) => (
+                                 <>
+                                    <p className="text-red-500">{desc.section}</p>
+                                    <p className="text-white">{desc.text}</p>
+                                 </>
+                              ))
+                           }
+                           <p></p>
+                        </div>
+                     </div>
+                     <div className="my-8">
+                        <Giscus
+                           id="comments"
+                           repo="ikhsanadrians/connexsoft-web"
+                           repoId="R_kgDOJwc6Ew"
+                           category="Announcements"
+                           categoryId=""
+                           mapping="specific"
+                           term="Welcome To Connexsoft Discussion"
+                           reactionsEnabled="1"
+                           emitMetadata="0"
+                           inputPosition="top"
+                           theme="dark_protanopia"
+                           lang="en"
+                           loading="lazy"
+                        />
+                     </div>
+                  </div>
+               ))
+            }
+         </div>
+         <Footer/>
       </>
    )
 
--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -31,12 +31,12 @@ const Articles = () => {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto mt-6 lg:py-0 lg:px-11 py-4 px-8 pb-8 relative">
+      <div className="container mx-auto mt-6 lg:py-0 lg:px-11 py-4 px-8 pb-8 relative mb-8">
         <h1 className="text-white font-[MonaReg] text-3xl lg:text-4xl">Articles</h1>
         <p className="text-gray-400 w-full lg:w-1/2">Welcome to ConnexSoft's article page, your gateway to captivating narratives and insightful content. Join us as we explore a world of engaging insights and thought-provoking perspectives.</p>
-        <div className="card-list grid grid-cols-3 gap-3">
+        <div className="card-list grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-3 pb-8">
           {
-            Articles.map((item: Articles, key: number) => (
+            Articles.map((item: Articles, key: number) => ( 
               <div onClick={()=>navigateToDetail(item.slug)} className="bg-[#161b21] relative border-[1.5px] z-99  border-[#2d3139] mt-6 rounded-md w-full h-full shadow-slate-600/50 shadow-2xl hover:scale-105 duration-200 flex flex-col  overflow-hidden cursor-pointer" key={key}>
                 <div className="img w-full h-36 object-cover">
                   <Image src={item.thumbnail} alt="thumbnail" width={900} height={700} className="w-full h-full object-cover" />
@@ -45,7 +45,7 @@ const Articles = () => {
                   <div className="article-description text-white">
                     {item.title}
                   </div>
-                  <div className="article-author text-slate-100 flex items-center gap-1">
+                  <div className="article-author text-slate-100 flex items-center gap-2">
                     <Image src={item.author_img} width={20} height={20} alt="author_img"/>
                     {item.author}
                   </div>
