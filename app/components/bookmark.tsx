f02947e59e0983745ac243673909e3cfcab9cb91
ikhsanadrians <aikhsan032@gmail.com>
2024-01-18 19:11:03 +0700

    fix: padding in bookmark

--- app/components/bookmark.tsx
+++ app/components/bookmark.tsx
@@ -4,7 +4,7 @@ import { FaInstagram } from "react-icons/fa";
 
 export default function Bookmark() {
     return ( 
-    <div className="h-12 lg:px-0 text-sm lg:text-base px-8 w-full flex justify-center gap-2 items-center bg-gradient-to-r from bg-red-500 to-red-700">
+    <div className="h-12 lg:px-0 text-sm lg:text-base px-2 w-full flex justify-center gap-2 items-center bg-gradient-to-r from bg-red-500 to-red-700">
         <h1 className="text-white font-semibold">Butuh Jasa Pembuatan Website & Aplikasi?</h1>
         <a href="https://www.instagram.com/tenizencode" target="_blank" className="tenizen-instagram hover:underline hover:underline-offset-2 duration-150 flex items-center gap-1 text-white">
             <FaInstagram size={17} color="white" />
