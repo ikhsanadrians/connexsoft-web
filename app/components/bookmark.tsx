e90c34c161920a8c5cb3f5cf5ff8d2eba2c1438e
ikhsanadrians <aikhsan032@gmail.com>
2024-01-18 14:28:03 +0700

    fix: bookmark header

--- app/components/bookmark.tsx
+++ app/components/bookmark.tsx
@@ -3,11 +3,12 @@ import { FaInstagram } from "react-icons/fa";
 
 
 export default function Bookmark() {
-    return <div className="h-12 w-full flex justify-center gap-2 items-center bg-red-500">
+    return ( <div className="h-12 w-full flex justify-center gap-2 items-center bg-gradient-to-r from bg-red-500 to-red-700">
         <h1 className="text-white font-semibold">Butuh Jasa Pembuatan Website & Aplikasi?</h1>
-        <a href="https://www.instagram.com/tenizencode" target="_blank" className="tenizen-instagram flex items-center gap-1 text-white">
-            <FaInstagram size={20} color="white" />
+        <a href="https://www.instagram.com/tenizencode" target="_blank" className="tenizen-instagram hover:underline hover:underline-offset-2 duration-150 flex items-center gap-1 text-white">
+            <FaInstagram size={17} color="white" />
             <p className="font-semibold">@tenizencode</p>
         </a>
-    </div>;
+    </div> 
+    )
 }
